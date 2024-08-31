11e8243fd5e0c369a5abc391fdd43aae4472445a
i-ftm <ftm.nateghi@gmail.com>
2024-08-31 23:19:43 +0330

    creat models

--- financial/Library_management/models.py
+++ financial/Library_management/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+class user(models.Model):
+    name = models.CharField("user name", max_length=100,blank=False,null=False)
+    email = models.EmailField("email address", blank=False)
+    password = models.CharField(max_length=100,blank=False,null=False)
+    
+class book(models.Model):
+    titel = models.CharField( max_length=250,blank=False,null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=3,blank=False,null=False)
+    author = models.CharField("author name", max_length=100,blank=False,null=False)
+    publicationdate = models.DateField(auto_now_add= True , null= False)
+    updated_at = models.DateField(auto_now = True , null= False)

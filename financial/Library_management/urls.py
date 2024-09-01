c45169a7d751e4cfcebe847386339729c6168e3c
i-ftm <ftm.nateghi@gmail.com>
2024-09-02 01:04:01 +0330

    v2

---
+++ financial/Library_management/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('api/books/',book_list,name='book_list')
+    path('api/books/add/',add_book,name='add_book')
+    path('api/books/search/',search_book,name='search_book')
+    path('api/books/edit/<int:bookID>/',edit_book,name='edit_book')
+    path('api/books/delete/<int:bookID>/',delete_book,name='delete_book')
+    path('api/books/filter/',fillter_book_list,name='fillter_book_list')
+    
+]

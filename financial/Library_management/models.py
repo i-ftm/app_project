dd55f518d18b16cb11f045ada977acd24a8cf77a
i-ftm <ftm.nateghi@gmail.com>
2024-09-02 00:36:42 +0330

    v2

--- financial/Library_management/models.py
+++ financial/Library_management/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-class user(models.Model):
+class User(models.Model):
     name = models.CharField("user name", max_length=100,blank=False,null=False)
     email = models.EmailField("email address", blank=False)
     password = models.CharField(max_length=100,blank=False,null=False)
     
-class book(models.Model):
-    titel = models.CharField( max_length=250,blank=False,null=False)
+class Book(models.Model):
+    title = models.CharField( max_length=250,blank=False,null=False)
     price = models.DecimalField(max_digits=10, decimal_places=3,blank=False,null=False)
     author = models.CharField("author name", max_length=100,blank=False,null=False)
     publicationdate = models.DateField(auto_now_add= True , null= False)

f1324fdc340c61d837c2953032e391e2738ce7b2
i-ftm <ftm.nateghi@gmail.com>
2024-09-02 00:54:48 +0330

    v2

--- financial/Library_management/views.py
+++ financial/Library_management/views.py
@@ -3,6 +3,7 @@ from .models import Book
 from django.db.models import Q
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.utils.dateparse import parse_date
 from django.utils.decorators import method_decorator
 import json
 
@@ -24,7 +25,7 @@ def add_book(request):
             'publication_date': book.publication_date.isoformat()
         }, status=201) #201 = added successfuly
         
-
+@csrf_exempt
 def book_list(request):
     books = Book.objects.all()
     book_list = [
@@ -59,15 +60,58 @@ def search_book(request):
 
 @csrf_exempt
 def edit_book(request , bookID):
-    book = get_object_or_404(Book, pk=bookID)
+    book = get_object_or_404(Book, pk = bookID)
     
+    if request.method == 'PUT':
+        data = json.loads(request.body)
+        book.title = data.get('title', book.title)
+        book.author = data.get('author', book.author)
+        book.price = data.get('price', book.price)
+        book.save()
+        
+        return JsonResponse({'message': 'Book updated successfully', 'book': {
+            'id': book.pk,
+            'title': book.title,
+            'author': book.author,
+            'price': str(book.price),
+            'publication_date': book.publication_date.isoformat()
+        }})
+
+    return JsonResponse({'error': 'Only PUT requests are allowed'}, status=400)
     
 
+@csrf_exempt
 def delete_book(request , bookID):
-    pass
+    book = get_object_or_404(Book, pk = bookID)
+    
+    if request.method == 'DELETE':
+        book.delete()
+        return JsonResponse({'message': 'Book deleted successfully'}, status=204)
 
-def fillter_book(request):
-    pass
+    return JsonResponse({'error': 'Only DELETE requests are allowed'}, status=400) 
+
+def fillter_book_list(request):
+    books = Book.objects.all()
+
+    price_limit = request.GET.get('price', None)
+    if price_limit is not None:
+        books = books.filter(price__lte=price_limit)
+
+    publication_date = request.GET.get('publication_date', None)
+    if publication_date is not None:
+        publication_date = parse_date(publication_date)
+        if publication_date:
+            books = books.filter(publication_date__gte=publication_date)
+
+    book_list = [{
+        'id': book.id,
+        'title': book.title,
+        'author': book.author,
+        'price': str(book.price),
+        'publication_date': book.publication_date.isoformat()
+    } for book in books]
+
+    return JsonResponse({'books': book_list})
 
 
 # Create your views here.

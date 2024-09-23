c024edab1a08a1992196ebac7db3e52287248873
i-ftm <ftm.nateghi@gmail.com>
2024-09-23 12:18:21 +0330

    dockerize

--- financial/Library_management/urls.py
+++ financial/Library_management/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import book_list, add_book, search_book, edit_book, delete_book,filter_book_list
 
 urlpatterns = [
-    path('books/',book_list,name='book_list'),
+    path('',book_list,name='book_list'),
     path('books/add/',add_book,name='add_book'),
     path('books/search/',search_book,name='search_book'),
     path('books/edit/<int:bookID>/',edit_book,name='edit_book'),
